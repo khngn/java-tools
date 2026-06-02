7b64052d8d39e64ccc930a6161f459590a11595c
Kim Nguyen <kim.h.nguyen@homeaffairs.gov.au>
2026-06-02 16:02:56 +1000

    Init SMimeGeneator

---
+++ smime-generator/src/main/java/devkn/jtools/SMimeGenerator.java
@@ -0,0 +1,9 @@
+package devkn.jtools;
+
+import java.util.Arrays;
+
+public class SMimeGenerator {
+    public static void main(String[] args) {
+        System.out.print("Hello: " + Arrays.toString(args));
+    }
+}
